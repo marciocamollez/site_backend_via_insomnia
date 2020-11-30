d1c3da93104fead0a5235f131a42d736bb8252bf
Marcio <marciocamollez@hotmail.com>
2020-11-30 11:06:44 -0300

    Configuracoes Iniciais

---
+++ src/app.js
@@ -0,0 +1,20 @@
+//Carregando os módulos - Jeito antigo
+//const express = require('express');
+//const routes = require('./routes');
+
+//Carregando os módulos - Jeito novo após instalar o sucrase
+import express from 'express';
+import routes from './routes';
+
+class App{
+    constructor(){
+        this.app = express();
+        this.routes();
+    }
+    routes(){
+        this.app.use(routes);
+    }
+}
+
+//module.exports = new App().app;
+export default new App().app;
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+const routes = new Router();
+
+
+routes.get('/', (req, res) => {
+    res.send("Server");
+})
+
+export default routes;
