e5719e2306fc193b479f5a973da94f090999cd02
Marcio <marciocamollez@hotmail.com>
2020-12-01 13:35:19 -0300

    Verificacao de cadastro de usuario'

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,53 @@
 import User from '../models/User';
 
 class UserController {
-    async store(req, res) {        
+    async store(req, res) {
+        const emailExiste = await User.findOne({ email: req.body.email });
+        if (emailExiste) {
+            return res.status(400).json({
+                error: true,
+                code: 102,
+                message: "Error: Este e-mail já está cadastrado!"
+            });
+        }
 
-        const user = await User.create(req.body);
-        return res.json(user);
+        if (!req.body.name || typeof req.body.name == undefined || req.body.name == null) {
+            return res.status(400).json({
+                error: true,
+                code: 103,
+                message: "Error: Necessário enviar o name com valor!"
+            });
+        }
+
+        if (!req.body.email || typeof req.body.email == undefined || req.body.email == null) {
+            return res.status(400).json({
+                error: true,
+                code: 104,
+                message: "Error: Necessário enviar o email com valor!"
+            });
+        }
+
+        if (!req.body.password || typeof req.body.password == undefined || req.body.password == null) {
+            return res.status(400).json({
+                error: true,
+                code: 105,
+                message: "Error: Necessário enviar o password com valor!"
+            });
+        }
+
+        const user = await User.create(req.body, (err) => {
+            if (err) return res.status(400).json({
+                error: true,
+                code: 101,
+                message: "Error: Usuário não foi cadastrado com sucesso!"
+            });
+
+            return res.status(200).json({
+                error: false,
+                message: "Usuário cadastrado com sucesso!",
+                dados: user
+            })
+        });
     }
 }
 
